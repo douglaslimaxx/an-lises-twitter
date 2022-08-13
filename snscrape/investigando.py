efa3b503a3f4a6231422a7253849aab6a6d06b22
Douglas Lima <douglas.lima@ccc.ufcg.edu.br>
2022-08-13 12:37:15 -0300

    adicionei tabela com cotagem dos rotulos em relacao aos termos

--- snscrape/investigando.py
+++ snscrape/investigando.py
@@ -4,34 +4,48 @@ import pandas as pd
 
 # dados = pd.read_csv("../../dados-tcc/word-"+palavra+".csv")
 
-dados = pd.read_csv("./dados/dados-finalizados.csv")
+# dados = pd.read_csv("./dados/dados-finalizadocs.csv")
+# dados = pd.read_csv("./dados/base-tcc.csv")
+dados = pd.read_csv("./dados/montados.csv")
 
-# dados_r = pd.read_csv("./dados/montados.csv")
+# toxidade = dados["toxidade"]
+# hate = dados["HateSpeech"]
+# palavras = dados["Palavra"].to_list()
 
-toxidade = dados["toxidade"]
-hate = dados["HateSpeech"]
-palavras = dados["Palavra"].to_list()
+tweets = dados["Texto"].to_list()
+rotulagem = dados["rotulagem"].to_list()
 
-print(len(palavras))
+# print(len(palavras))
 
 termos = ["baitola", "bichona", "bixa", "boiola", "gayzada",
           "gayzismo", "homossexualismo", "viadagem", "viadao", "viadinho", "bicha", "viado"]
 
-for t in termos:
-    print("Foram %i tweets com a palavra %s" % (palavras.count(t), t))
+c = 0
+h = 0
+n = 0
+for i in range(len(tweets)):
+    if('viado' in tweets[i].split()):
+        c += 1
+        if(rotulagem[i] == "yes"):
+            h += 1
+        else :
+            n +=1
+print("Foram %i tweets com a palavra %s" % (c, 'viado'))
+print("Foram %i tweets homofobicos com a palavra %s" % (h, 'viado'))
+print("Foram %i tweets não homofóbicos com a palavra %s" % (n, 'viado'))
 
-toxic_hate = 0
-is_hate_notToxic = 0
+# toxic_hate = 0
+# is_hate_notToxic = 0
 
-for t in range(len(toxidade)):
-    if(toxidade[t] == 1):
-        toxic_hate += 1
-    if(hate[t] == "yes"):
-        is_hate_notToxic += 1
+# for t in range(len(toxidade)):
+#     if(toxidade[t] == 1):
+#         toxic_hate += 1
+#     if(hate[t] == "yes"):
+#         is_hate_notToxic += 1
 
-print("O número de tweets tóxicos foi %i" % toxic_hate)
-print("O número de tweets com discurso de ódio foi %i" %
-      is_hate_notToxic)
+# print("O número de tweets tóxicos foi %i" % toxic_hate)
+# print("O número de tweets com discurso de ódio foi %i" %
+#       is_hate_notToxic)
 
 # del dados["Unnamed: 0.2"]
 
